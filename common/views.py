100e47743791cf849cb615fefe463d2203ef01e5
Lektor <denislour@gmail.com>
2022-04-25 23:52:57 +0700

    Add new login api feature.

--- common/views.py
+++ common/views.py
@@ -1,7 +1,8 @@
-from rest_framework.exceptions import APIException
+from rest_framework.exceptions import APIException, AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.models import User
 from .serializers import UserSerializer
 
 
@@ -16,3 +17,17 @@ class RegisterAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect Password!')
+        return Response(UserSerializer(user).data)
