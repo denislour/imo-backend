af77b290f02286b7777c1fed21f9ed2827be9f85
Lektor <denislour@gmail.com>
2022-04-28 22:57:50 +0700

    Update user API

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, LogoutAPIView, UserAPIView
+from .views import RegisterAPIView, LoginAPIView, LogoutAPIView, UserAPIView, ProfileInfoAPIView, ProfilePasswordAPIView
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view()),
     path('login', LoginAPIView.as_view()),
     path('logout', LogoutAPIView.as_view()),
     path('user', UserAPIView.as_view()),
+    path('user/info', ProfileInfoAPIView.as_view()),
+    path('user/password', ProfilePasswordAPIView.as_view()),
 ]
--- common/views.py
+++ common/views.py
@@ -59,3 +59,30 @@ class LogoutAPIView(APIView):
             'message': 'success'
         }
         return response
+
+
+class ProfileInfoAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk=None):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class ProfilePasswordAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk=None):
+        user = request.user
+        data = request.data
+
+        if data.get('password') != data.get('password_confirm'):
+            raise APIException('Password do not match!')
+        user.set_password(data.get('password'))
+        user.save()
+        return Response(UserSerializer(user).data)
