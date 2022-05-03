c6e01968d559430768d4b3595ae824e5efd2b4b6
Lektor <denislour@gmail.com>
2022-05-03 18:59:30 +0700

    [Admin] - Refactor and update authenticated for get ambassador API

--- administrator/views.py
+++ administrator/views.py
@@ -1,11 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from common.serializers import UserSerializer
 from core.models import User
 
+from common.authen import JWTAuthentication
+
 
 class AmbassadorAPIView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, _):
         ambassadors = User.objects.filter(is_ambassador=True)
